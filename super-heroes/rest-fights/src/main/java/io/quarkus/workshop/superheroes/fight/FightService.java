9657b5b23e71ad1dd673f360714d1e61818c7ed3
Wellinton Silveira <wellinton.silveira@syonet.com>
2026-03-20 16:45:25 -0300

    Microserviços funcionando e lutas sendo realizadas

---
+++ super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightService.java
@@ -0,0 +1,114 @@
+package io.quarkus.workshop.superheroes.fight;
+
+import io.quarkus.workshop.superheroes.fight.client.Hero;
+import io.quarkus.workshop.superheroes.fight.client.HeroProxy;
+import io.quarkus.workshop.superheroes.fight.client.Villain;
+import io.quarkus.workshop.superheroes.fight.client.VillainProxy;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Random;
+
+import static jakarta.transaction.Transactional.TxType.REQUIRED;
+import static jakarta.transaction.Transactional.TxType.SUPPORTS;
+
+@ApplicationScoped
+@Transactional( SUPPORTS )
+public class FightService {
+
+    @RestClient
+    HeroProxy heroProxy;
+    @RestClient
+    VillainProxy villainProxy;
+
+    @Inject
+    Logger logger;
+
+    private final Random random = new Random();
+
+    public List< Fight > findAllFights() {
+        return Fight.listAll();
+    }
+
+    public Fight findFightById( Long id ) {
+        return Fight.findById( id );
+    }
+
+    Villain findRandomVillain() {
+        return villainProxy.findRandomVillain();
+    }
+
+    Hero findRandomHero() {
+        return heroProxy.findRandomHero();
+    }
+
+    Fighters findRandomFighters() {
+        Hero hero = findRandomHero();
+        Villain villain = findRandomVillain();
+        Fighters fighters = new Fighters();
+        fighters.hero = hero;
+        fighters.villain = villain;
+        return fighters;
+    }
+
+    @Transactional( REQUIRED )
+    public Fight persistFight( Fighters fighters ) {
+        Fight fight;
+
+        int heroAdjust = random.nextInt( 20 );
+        int villainAdjust = random.nextInt( 20 );
+
+        if ( ( fighters.hero.level + heroAdjust )
+            > ( fighters.villain.level + villainAdjust ) ) {
+            fight = heroWon( fighters );
+        } else if ( fighters.hero.level < fighters.villain.level ) {
+            fight = villainWon( fighters );
+        } else {
+            fight = random.nextBoolean() ? heroWon( fighters ) : villainWon( fighters );
+        }
+
+        fight.fightDate = Instant.now();
+        fight.persist();
+
+        return fight;
+    }
+
+    private Fight heroWon( Fighters fighters ) {
+        logger.info( "Yes, Hero won :o)" );
+        Fight fight = new Fight();
+        fight.winnerName = fighters.hero.name;
+        fight.winnerPicture = fighters.hero.picture;
+        fight.winnerLevel = fighters.hero.level;
+        fight.winnerPowers = fighters.hero.powers;
+        fight.loserName = fighters.villain.name;
+        fight.loserPicture = fighters.villain.picture;
+        fight.loserLevel = fighters.villain.level;
+        fight.loserPowers = fighters.villain.powers;
+        fight.winnerTeam = "heroes";
+        fight.loserTeam = "villains";
+        return fight;
+    }
+
+    private Fight villainWon( Fighters fighters ) {
+        logger.info( "Gee, Villain won :o(" );
+        Fight fight = new Fight();
+        fight.winnerName = fighters.villain.name;
+        fight.winnerPicture = fighters.villain.picture;
+        fight.winnerLevel = fighters.villain.level;
+        fight.winnerPowers = fighters.villain.powers;
+        fight.loserName = fighters.hero.name;
+        fight.loserPicture = fighters.hero.picture;
+        fight.loserLevel = fighters.hero.level;
+        fight.loserPowers = fighters.hero.powers;
+        fight.winnerTeam = "villains";
+        fight.loserTeam = "heroes";
+        return fight;
+    }
+
+}
